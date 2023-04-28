b86dbfb8056eb5c84ce5c07de0a84ef193b550d2
Funsho <funshoajayi29@gmail.com>
2023-04-28 18:43:02 +0100

    fixed error with mapping and post id

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,13 @@
 import Header from "@/components/Header";
 
+export const getStaticPaths = async =>{
+
+}
+
 const PostDetasils = () => {
     return ( <div>
 {/* header */}
-<Header btnName={'All Posts'} route={'posts'}/>
+<Header btnName={'Back'} route={'posts'}/>
 <main className="min-h-screen">
      <h1>deets</h1>
 </main>
